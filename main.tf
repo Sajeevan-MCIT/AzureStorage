6c28f94524956495da15aeb8b0f0fa5afdd50048
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-09-13 19:58:57 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_resource_group"
   location = "Canada Central"
 }
+
+resource "azurerm_storage_account" "azureresourcegroup" {
+  name                     = "MCIT_storage_account"
+  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
+  location                 = azurerm_resource_group.azureresourcegroup.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
