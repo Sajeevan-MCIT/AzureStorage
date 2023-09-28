99c5fad330cb596994ea2734a302bda07b149b09
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-09-27 20:37:42 -0400

    Update main.tf
    dd

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  cluster_names = ["Saj","Paul","Ladi","Emmanuel","Douglas","Jade","Ola"]
+  #cluster_names = ["Saj","Paul","Ladi","Emmanuel","Douglas","Jade","Ola"]
   whatever= ["bob","smith","bart","homer"]
 }
 
@@ -8,23 +8,6 @@ resource "azurerm_resource_group" "classwork2509" {
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "myk8s" {
-  for_each            = { for cluster in local.cluster_names : cluster => cluster }
-  name                = "k8s-${var.convention}-${each.key}"
-  location            = azurerm_resource_group.classwork2509.location
-  resource_group_name = azurerm_resource_group.classwork2509.name
-  dns_prefix          = "${var.prefix}-k8s"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
 
 resource "azurerm_storage_blob" "classwork2509" {
   for_each            = { for brap in local.whatever : brap => brap }  
