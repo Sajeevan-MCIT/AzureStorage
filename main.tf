dce518ac78df0eb0569514ffc4d16a33ec8b0608
Sajeevan-MCIT <126923381+Sajeevan-MCIT@users.noreply.github.com>
2023-09-27 20:34:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,36 @@
-#This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
-
-resource "azurerm_resource_group" "azureresourcegroup" {
-  name     = "MCIT_resource_group"
-  location = "Canada Central"
+locals {
+  cluster_names = ["Saj","Paul","Ladi","Emmanuel","Douglas","Jade","Ola"]
+  whatever= ["bob","smith","bart","homer"]
 }
 
-resource "azurerm_storage_account" "azureresourcegroup" {
-  name                     = "saj1storage1account"
-  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
-  location                 = azurerm_resource_group.azureresourcegroup.location
-  account_tier             = var.account_tier
-  account_replication_type = "GRS"
+resource "azurerm_resource_group" "classwork2509" {
+  name     = "rg-${var.convention}"
+  location = var.location
+}
 
-  tags = {
-    environment = "staging"
+resource "azurerm_kubernetes_cluster" "myk8s" {
+  for_each            = { for cluster in local.cluster_names : cluster => cluster }
+  name                = "k8s-${var.convention}-${each.key}"
+  location            = azurerm_resource_group.classwork2509.location
+  resource_group_name = azurerm_resource_group.classwork2509.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_storage_blob" "classwork2509" {
+  for_each            = { for brap in local.whatever : brap => brap }  
+  name                   = "$(each.key)"
+  storage_account_name   = azurerm_storage_account.classwork2509.name
+  storage_container_name = azurerm_storage_container.classwork2509.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
 }
